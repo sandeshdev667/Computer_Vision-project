fd808e922b3bc6a9a315787051d5610bfa25c834
sandeshdev667 <sxa7784@mavs.uta.edu>
2025-11-20 01:41:53 -0600

    Add files via upload

--- Color_Detector/main.py
+++ Color_Detector/main.py
@@ -1,23 +1,30 @@
 import cv2
+from PIL import Image
 from range_file import get_limits
 
-red = [0, 0, 255]  # BGR for red
-
-
+yellow = [0, 255, 255]  # yellow in BGR colorspace
 cap = cv2.VideoCapture(0)
-while True: 
+while True:
     ret, frame = cap.read()
 
-    hsvImage= cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    lowerLimit, upperLimit = get_limits(red)
+    lowerLimit, upperLimit = get_limits(color=yellow)
 
-    mask = cv2.inRange(hsvImage, (lowerLimit), (upperLimit))
+    mask = cv2.inRange(hsvImage, lowerLimit, upperLimit)
 
+    mask_ = Image.fromarray(mask)
 
-    cv2.imshow('Red_Color_Detection   (Press "a" to quit)', mask)
+    bbox = mask_.getbbox()
 
-    if cv2.waitKey(1) & 0xFF == ord('a'):
+    if bbox is not None:
+        x1, y1, x2, y2 = bbox
+
+        frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
+
+    cv2.imshow('frame', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
